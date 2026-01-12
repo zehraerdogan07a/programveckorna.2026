7bac9d2f1ffbe888916c9191036ecf39ef8573ce
lightningmcqueen69 <lou.figueroa@elev.ga.lbs.se>
2026-01-12 12:10:38 +0100

    lifebar

---
+++ programveckorna2026/Assets/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int lives = 3;
+    public LifeUI lifeUI;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        lifeUI.UpdateHearts(lives);
+    }
+
+    public void LoseLife()
+    {
+        lives--;
+
+        if (lives < 0)
+            lives = 0;
+
+        lifeUI.UpdateHearts(lives);
+
+        if (lives == 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+
+    // TEMPORARY TEST
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoseLife();
+        }
+    }
+}
+
