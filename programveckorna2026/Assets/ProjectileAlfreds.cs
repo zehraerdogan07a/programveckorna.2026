b795138db67e0c97d88b499716bf8de560293ba2
Alfr333d <151173496+Alfr333d@users.noreply.github.com>
2026-01-13 09:56:25 +0100

    enemy lätt

---
+++ programveckorna2026/Assets/ProjectileAlfreds.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ProjectileAlfreds : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
